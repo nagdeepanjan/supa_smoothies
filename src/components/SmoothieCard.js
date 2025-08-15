78b1b3a88cbfa5d7493af5f0c5b2f191d27af8e9
nagdeepanjan <mail@deepanjannag.com>
2025-08-15 13:41:56 -0400

    Fully Functional

---
+++ src/components/SmoothieCard.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
+
+export default function SmoothieCard({smoothie, onDelete}){
+    async function  handleDelete(e){
+        const {data, error} = await supabase
+            .from('smoothies')
+            .delete()
+            .eq('id', smoothie.id)
+            .select()
+
+        if(error){
+            console.log('Error while deleting: ', error)
+        }
+        if(data){
+            console.log('Deleted successfully: ', data)
+            onDelete(smoothie.id)
+        }
+    }
+    
+    
+    
+    
+    return(
+        <div className="smoothie-card">
+            <h3>{smoothie.title}</h3>
+            <p>{smoothie.method}</p>
+            <div className="rating">{smoothie.rating}</div>
+            <div className="buttons">
+                <Link to={'/' + smoothie.id}>
+                    <i className="material-icons">edit</i>
+                </Link>
+                <i className="material-icons" onClick={handleDelete}>delete</i>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,70 @@
-const Home = () => {
+import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
+import SmoothieCard from "../components/SmoothieCard"
+
+export default function Home() {
+
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at')
+
+  function handleDelete(id){
+    setSmoothies(prevSmoothies=>{
+      return prevSmoothies.filter(s=>s.id!=id)
+    })
+  }
+
+
+  useEffect(()=>{
+    const fetchSmoothies = async() =>{
+      const {data, error} = await supabase
+      .from('smoothies')
+      .select()
+      .order(orderBy, {ascending: false})
+
+      if(error)
+      {
+        setFetchError('Could not fetch smoothies')
+        setSmoothies(null)
+        console.log(error)
+      }
+      if(data)
+      {
+        console.log(data)
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+    fetchSmoothies()
+  }, [orderBy])
+
   return (
+  
     <div className="page home">
-      <h2>Home</h2>
+      <h1>Smoothies</h1>
+
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <div className="smoothies">
+
+          <div className="order-by">
+            <p>Order By:</p>
+            <button onClick={()=>setOrderBy('created_at')}>Time Created</button>
+            <button onClick={()=>setOrderBy('title')}>Title</button>
+            <button onClick={()=>setOrderBy('rating')}>Rating</button>
+          </div>
+
+
+          <div className="smoothie-grid">
+            {
+              smoothies.map(s=>(
+                <SmoothieCard key = {s.id} smoothie={s} onDelete={handleDelete}/>
+              ))
+            }
+          </div>
+          
+        </div>
+      )}
     </div>
   )
 }
-
-export default Home
